8198b793fcfeac157873a1361f62188bc969e3a7
dmig <dmig@localhost>
2007-07-14 13:28:10 +0000

    lcd flickering fixed, cleanup

--- arch/arm/mach-pxa/loox720/loox720_lcd.c
+++ arch/arm/mach-pxa/loox720/loox720_lcd.c
@@ -1,10 +1,10 @@
 /*
- * LCD driver for Dell Axim X30
- * 
- * Authors: Giuseppe Zompatori <giuseppe_zompatori@yahoo.it>
- * 
+ * LCD driver for Fujitsu-Siemens Loox 720
+ *
+ * Authors: ...
+ *
  * based on previous work, see below:
- * 
+ *
  * Machine initialization for Dell Axim X3
  *
  * Authors: Andrew Zabolotny <zap@homelink.ru>
@@ -37,40 +37,26 @@
 #include <linux/corgi_bl.h>
 
 
-static struct pxafb_mode_info loox720_lcd_modes[] = {
-{
-	.pixclock = 96153,
-	.bpp = 16,
-	.xres = 480,
-	.yres = 640,
-	.hsync_len = 4,
-	.vsync_len = 1,
-	.left_margin = 20,
-	.upper_margin = 8,
-	.right_margin = 8,
-	.lower_margin = 9,
-//	.sync = 0,
-/*	.pixclock = 96153,
-	.bpp = 16,
-	.xres = 480,
-	.yres = 640,
-	.hsync_len = 64,
-	.vsync_len = 5,
-	.left_margin = 17,
-	.upper_margin = 1,
-	.right_margin = 87,
-	.lower_margin = 4,
-//	.sync = 0,*/
-}
+static struct pxafb_mode_info loox720_lcd_mode_info = {
+	.pixclock		= 57692,
+	.bpp			= 16,
+	.xres			= 480,
+	.yres			= 640,
+	.hsync_len		= 4,
+	.left_margin		= 20,
+	.right_margin		= 8,
+	.vsync_len		= 1,
+	.upper_margin		= 8,
+	.lower_margin		= 9,
+	.sync			= 0,
 };
 
 static struct pxafb_mach_info loox720_fb_info =
 {
-    .modes		= loox720_lcd_modes,
-    .num_modes	= ARRAY_SIZE(loox720_lcd_modes), 
-
-	.lccr0 = 0x001000f9,
-	.lccr3 = 0x04700007
+	.modes		= &loox720_lcd_mode_info,
+	.num_modes	= 1,
+	.lccr0		= LCCR0_Act | LCCR0_Sngl | LCCR0_Color,
+	.lccr3		= LCCR3_OutEnH | LCCR3_PixRsEdg,
 };
 
 static int loox720_lcd_set_power(struct lcd_device *dev, int power)
@@ -85,7 +71,6 @@ static int loox720_lcd_get_power(struct lcd_device *dev)
 
 static struct lcd_ops loox720_lcd_properties =
 {
-//	.owner = THIS_MODULE,
 	.set_power = loox720_lcd_set_power,
 	.get_power = loox720_lcd_get_power,
 	/* @@ more here @@ */
@@ -101,7 +86,7 @@ loox720_lcd_init (void)
 
 	set_pxa_fb_info(&loox720_fb_info);
 	pxafb_lcd_device = lcd_device_register("pxafb", NULL, &loox720_lcd_properties);
-	
+
 //	if (IS_ERR (pxafb_lcd_device))
 //		return PTR_ERR (pxafb_lcd_device);
 	//possible BUG location, dunno what to pass as 2nd param
@@ -125,7 +110,7 @@ loox720_lcd_exit (void)
 module_init (loox720_lcd_init);
 module_exit (loox720_lcd_exit);
 
-MODULE_AUTHOR("Giuseppe Zompatori <giuseppe_zompatori@yahoo.it>");
+MODULE_AUTHOR("...");
 MODULE_DESCRIPTION("Loox 720 Core frambuffer driver");
 MODULE_LICENSE("GPL");
 
