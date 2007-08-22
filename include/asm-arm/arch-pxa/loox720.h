dbbecc897aa8b7cc5db75af4df6f281db20efa63
tom3q <tom3q@localhost>
2007-08-22 22:00:10 +0000

    LEDS related

--- include/asm-arm/arch-pxa/loox720.h
+++ include/asm-arm/arch-pxa/loox720.h
@@ -27,3 +27,16 @@ struct loox720_bt_funcs {
 
 #endif /* _LOOX720_H_ */
 
+/*
+ * Loox 720 Leds
+ */
+
+#define LOOX720_LED_LEFT 0
+#define LOOX720_LED_RIGHT 1
+#define LOOX720_LED_COLOR_A 1
+#define LOOX720_LED_COLOR_B 2
+#define LOOX720_LED_BLINK 4
+#define LOOX720_LED_BLINK_ALT 8
+
+extern void loox720_enable_led(int led, int mode);
+extern void loox720_disable_led(int led, int mode);
